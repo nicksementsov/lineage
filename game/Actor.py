a724175048cd44d0ff0a27e6a672051c762a91ab
Nick S <nickolaisementsov@gmail.com>
2017-10-13 18:38:16 -0400

    Finished skeleton

--- game/Actor.py
+++ game/Actor.py
@@ -2,7 +2,9 @@ from pygame import sprite
 
 class Actor(sprite.Sprite):
     """description of class"""
-    def __init__(self, x, y):
+    def __init__(self, _x, _y):
         sprite.Sprite.__init__(self)
+        self.x = _x
+        self.y = _y
 
 
--- lineage.py
+++ lineage.py
@@ -1,15 +1,34 @@
 import pygame
 
-import game
+from game import Actor
 
 SCREEN_W = 1280
 SCREEN_H = 720
+MAX_FPS = 120
+
+C_BLACK = (0, 0, 0)
 
 pygame.init()
 
-a = game.Actor()
+Player = Actor.Actor(0, 0)
 
-current_time = pygame.time.Clock
-main_window = pygame.display.set_mode((SCREEN_W, SCREEN_H), pygame.OPENGL | pygame.DOUBLEBUF | pygame.RESIZABLE)
+game_clock = pygame.time.Clock()
+main_window = pygame.display.set_mode((SCREEN_W, SCREEN_H), pygame.DOUBLEBUF | pygame.RESIZABLE)
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False;
+
+    main_window.fill(C_BLACK)
+
+    # RENDER
+
+    pygame.display.flip()
+    game_clock.tick(MAX_FPS)
+
 
 pygame.quit()
